02b0b3598db9d197b8bb30ad23bfe72130a48505
sebastianh2112 <sebastianher2112@hotmail.com>
2026-06-25 15:22:29 -0600

    Fix: serve media files in non-debug mode for demo

--- backend/WebDjango/urls.py
+++ backend/WebDjango/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('pagos/', include('MetodoPago.urls')),
     path('api/', include('api.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
